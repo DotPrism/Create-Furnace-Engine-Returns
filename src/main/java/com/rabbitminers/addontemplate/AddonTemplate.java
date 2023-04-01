bac7d3837edcbda3373d68d51e861c5ac335b0c6
MagneticPrism <116608284+DotPrism@users.noreply.github.com>
2023-03-31 23:09:56 -0400

    Update AddonTemplate.java

--- src/main/java/com/rabbitminers/addontemplate/AddonTemplate.java
+++ src/main/java/com/rabbitminers/addontemplate/AddonTemplate.java
@@ -1,9 +1,9 @@
 package com.rabbitminers.addontemplate;
 
 import com.mojang.logging.LogUtils;
-import com.rabbitminers.addontemplate.index.AddonBlocks;
-import com.rabbitminers.addontemplate.index.AddonItems;
-import com.rabbitminers.addontemplate.index.AddonTileEntities;
+import com.magneticprism.furnaceenginereturns.index.AddonBlocks;
+import com.magneticprism.furnaceenginereturns.index.AddonItems;
+import com.magneticprism.furnaceenginereturns.index.AddonTileEntities;
 import com.simibubi.create.AllBlocks;
 import com.simibubi.create.foundation.data.CreateRegistrate;
 import com.tterrag.registrate.util.nullness.NonNullSupplier;
@@ -27,10 +27,10 @@ import org.slf4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(AddonTemplate.MODID)
-public class AddonTemplate {
+@Mod(FurnaceEngineReturns.MODID)
+public class FurnaceEngineReturns {
     public static final String MODID = "furnaceenginereturns";
-    private static final NonNullSupplier<CreateRegistrate> registrate = CreateRegistrate.lazy(AddonTemplate.MODID);
+    private static final NonNullSupplier<CreateRegistrate> registrate = CreateRegistrate.lazy(FurnaceEngineReturns.MODID);
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
 
@@ -42,7 +42,7 @@ public class AddonTemplate {
         }
     };
 
-    public AddonTemplate()
+    public FurnaceEngineReturns()
     {
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
